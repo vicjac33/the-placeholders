0a520d3458b5a5ee22d38d8ba77e68b087ddf486
Mark Gardner <mark.gardner@endurance.com>
2019-01-17 20:02:04 -0600

    phase 3

--- main.js
+++ main.js
@@ -1,37 +1,52 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCBWMFIeBeUSc1wRrp8_JJ7ME29-Qo0Ivo",
-    authDomain: "timesheet-77e67.firebaseapp.com",
-    databaseURL: "https://timesheet-77e67.firebaseio.com",
-    projectId: "timesheet-77e67",
-    storageBucket: "timesheet-77e67.appspot.com",
-    messagingSenderId: "900015543508"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCBWMFIeBeUSc1wRrp8_JJ7ME29-Qo0Ivo",
+  authDomain: "timesheet-77e67.firebaseapp.com",
+  databaseURL: "https://timesheet-77e67.firebaseio.com",
+  projectId: "timesheet-77e67",
+  storageBucket: "timesheet-77e67.appspot.com",
+  messagingSenderId: "900015543508"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-$(document).ready(function(){
+$(document).ready(function() {
+  $("#add-employee").on("click", function(event) {
+    event.preventDefault();
 
-    $("#add-employee").on("click", function(event){
-       
-        event.preventDefault();
+    var name = $("#name-input")
+      .val()
+      .trim();
+    var role = $("#role-input")
+      .val()
+      .trim();
+    var startDate = $("#date-input")
+      .val()
+      .trim();
+    var monthlyRate = $("#rate-input")
+      .val()
+      .trim();
 
-            var name = $("#name-input").val().trim();
-            var role = $("#role-input").val().trim();
-            var startDate = $("#date-input").val().trim();
-            var monthlyRate = $("#rate-input").val().trim();
-
-            database.ref().push({
-
-                name: name,
-                role: role, 
-                startDate: startDate,
-                monthlyRate: monthlyRate,
-                dateAdded: firebase.database.ServerValue.TIMESTAMP
-
-            });
-
+    database.ref().push({
+      name: name,
+      role: role,
+      startDate: startDate,
+      monthlyRate: monthlyRate,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
+  });
 
+  database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
+    var sv = snapshot.val();
+    var nameTh = $("<th>").attr("scope", "row").text(sv.name);
+    var roleTd = $("<td>").text(sv.role);
+    var startDateTd = $("<td>").text(sv.startDate);
+    var monthsWorkedTd = $("<td>");
+    var monthlyRateTd = $("<td>").text(sv.monthlyRate);
+    var totalBilledTd = $("<td>");
+
+    var tr = $("<tr>").append(nameTh, roleTd, startDateTd, monthsWorkedTd, monthlyRateTd, totalBilledTd);
+    $("#employees").append(tr);
+  });
 });
