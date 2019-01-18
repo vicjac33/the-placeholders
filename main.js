575a2a70c910350938a06967f332e809c1c64155
Aaron <aarongladstone456@gmail.com>
2019-01-17 19:38:46 -0600

    phase 2

---
+++ main.js
@@ -0,0 +1,37 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCBWMFIeBeUSc1wRrp8_JJ7ME29-Qo0Ivo",
+    authDomain: "timesheet-77e67.firebaseapp.com",
+    databaseURL: "https://timesheet-77e67.firebaseio.com",
+    projectId: "timesheet-77e67",
+    storageBucket: "timesheet-77e67.appspot.com",
+    messagingSenderId: "900015543508"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+$(document).ready(function(){
+
+    $("#add-employee").on("click", function(event){
+       
+        event.preventDefault();
+
+            var name = $("#name-input").val().trim();
+            var role = $("#role-input").val().trim();
+            var startDate = $("#date-input").val().trim();
+            var monthlyRate = $("#rate-input").val().trim();
+
+            database.ref().push({
+
+                name: name,
+                role: role, 
+                startDate: startDate,
+                monthlyRate: monthlyRate,
+                dateAdded: firebase.database.ServerValue.TIMESTAMP
+
+            });
+
+    });
+
+});
